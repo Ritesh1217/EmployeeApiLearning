29fd992267a6646a7d3be96ce6583e9086f2055a
Ritesh Kumar Singh <ritesh@surelia.info>
2026-08-05 14:02:09 +0530

    implemented authRepository and interface of it

---
+++ EmployeeApiLearning/Repositories/AuthRepository.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeApiLearning.Data;
+using EmployeeApiLearning.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApiLearning.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AuthRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<AppUser?> GetUserByUsername(string username)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.Username == username);
+        }
+        public async Task<AppUser?> GetUserByEmployeeCode(string employeeCode)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(x => x.EmployeeCode == employeeCode); 
+        }
+        public async Task AddUser(AppUser user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ EmployeeApiLearning/Repositories/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeApiLearning.Models;
+
+namespace EmployeeApiLearning.Repositories
+{
+    public interface IAuthRepository
+    {
+        Task<AppUser?> GetUserByUsername(string username);
+        Task<AppUser?> GetUserByEmployeeCode(string employeeCode);
+        Task AddUser(AppUser user);
+        Task SaveChanges();
+    }
+}
